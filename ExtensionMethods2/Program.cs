9514ac9f3de328781a5c07f06a0dd7a088daf87d
Bruno Amaral <bruno_henrique_a@hotmail.com>
2019-09-10 22:37:35 -0300

    segundo exemplo de uso de extension methods

---
+++ ExtensionMethods2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExtensionMethods2.Extensions;
+
+namespace ExtensionMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dears!";
+            Console.WriteLine(s1.Cut(10));   
+        }
+    }
+}
